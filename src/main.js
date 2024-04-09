3897948a73206bfedba5008f15d549483a76e8e5
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-04-09 15:11:15 +0330

    refactor: use jquery

---
+++ src/main.js
@@ -0,0 +1,8 @@
+"use strict"
+
+import '../node_modules/jquery/dist/jquery.min.js';
+
+
+$('#navbar').load('./Component/Main/NavBar/NavBar.html #component-navbar');
+
+$('#home').load('./Pages/Home/Home.html #page-home');
